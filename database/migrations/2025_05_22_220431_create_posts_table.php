fb85830635bfc1c1a19c09f9f99abbbd2e497bc3
Matias-Ignacio <matias.riveiro@est.fi.uncoma.edu.ar>
2025-05-27 19:53:39 -0300

    Tablas nuevas

--- database/migrations/2025_05_22_220431_create_posts_table.php
+++ database/migrations/2025_05_22_220431_create_posts_table.php
@@ -11,14 +11,23 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->timestamps();
+        });
+
         Schema::create('posts', function (Blueprint $table) {
             $table->id(); //id autoincremental.
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('categories_id')->constrained()->onDelete('cascade');
             $table->string('title'); //Titulo
             $table->string('poster'); //Imagen o autor.
             $table->boolean('habilitated')->default(false); //Habilitado, por defecto es falso.
             $table->text('content'); //Contenido largo.
             $table->timestamps(); //created_at y updated_at
         });
+
     }
 
     /**
@@ -27,5 +36,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('posts');
+        Schema::dropIfExists('categories');
     }
 };
