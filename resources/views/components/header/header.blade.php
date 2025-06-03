9fbc30cc299a07ac71ed0466d7e9cef2d9de9275
Dario Fuentealba <dario.fuentealba@est.fi.uncoma.edu.ar>
2025-06-03 16:11:30 -0300

    botones header

--- resources/views/components/header/header.blade.php
+++ resources/views/components/header/header.blade.php
@@ -8,23 +8,31 @@
             @php
                 ob_start();
             @endphp
+<div class="flex items-center flex-col md:flex-row md:justify-evenly w-full md:ml-auto space-y-2 md:space-y-0">
 
-            <div class="flex items-center flex-col md:flex-row md:space-x-4 md:ml-auto space-y-2 md:space-y-0">
+{{--<div class="flex items-center flex-col md:flex-row md:space-x-4 md:ml-auto space-y-2 md:space-y-0">--}}
+                <!-- Inicio -->
                 <x-botones.boton-header texto="Inicio" onclick="window.location.href='{{ url('/') }}'" />
+                <!-- Categorias -->
                 <x-botones.boton-header texto="Categorías" onclick="window.location.href='{{ url('/category') }}'" />
 
                 @guest
+                    <!-- Registrarse -->
                     <x-botones.boton-sesion  onclick="window.location.href='{{ url('/register') }}'" imagenUrl="https://images.icon-icons.com/3478/PNG/512/register_login_signup_icon_219991.png"  tooltip="Registrarse" />
+                    <!-- Login -->
                     <x-botones.boton-sesion onclick="window.location.href='{{ url('/login') }}'" imagenUrl="https://images.icon-icons.com/2406/PNG/512/user_account_icon_145918.png"  tooltip="Login" />
 
                 @else
                     <!-- Favoritos -->
                     <x-botones.boton-header texto="Favoritos" onclick="window.location.href='{{ url('/favorites') }}'"></x-botones.boton-header>
                     
+                    <!-- Mis categorias -->
                     <x-botones.boton-header texto="Mis Categorías" onclick="window.location.href='{{ url('/category/userCategories') }}'" />
+                    <!-- Perfil -->
                     <x-botones.boton-header texto="Perfil" onclick="window.location.href='{{ route('profile.show') }}'" />
+                    <!-- Cerrar sesion -->
                     <x-botones.boton-sesion 
-                        imagenUrl="https://images.icon-icons.com/2518/PNG/512/logout_icon_151219.png"
+                        imagenUrl="https://images.icon-icons.com/2518/PNG/512/logout_icon_151219.png" tooltip="Logout" 
                         onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                     </x-botones.boton-sesion>
 
