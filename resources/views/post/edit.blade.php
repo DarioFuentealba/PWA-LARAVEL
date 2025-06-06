a295024980c140ce4d617769be5d79af8d9380be
Dario Fuentealba <dario.fuentealba@est.fi.uncoma.edu.ar>
2025-06-06 18:45:06 -0300

    Estilo editar post

--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -1,6 +1,7 @@
 @extends('layout')
 
 @section('content')
+<x-guest-layout>
     <x-titulo.titulo>Editar post</x-titulo.titulo>
 
     <h1 class="text-2xl font-bold mb-4">Editar Post</h1>
@@ -10,30 +11,31 @@
         @method('PUT')
 
         <div>
-            <label class="block font-semibold">Título</label>
-            <input type="text" name="title" value="{{ $post->title }}" class="w-full border px-2 py-1">
+            <label class="text-[#0d1b2a] block font-semibold">Título</label>
+            <input type="text" name="title" value="{{ $post->title }}" class="px-2 py-1 border-[#0d1b2a] focus:border-[#0d1b2a] focus:ring-[#0d1b2a] rounded-md shadow-sm mt-1 block w-full">
         </div>
 
         <div>
-            <label class="block font-semibold">Poster</label>
-            <input type="text" name="poster" value="{{ $post->poster }}" class="w-full border px-2 py-1">
+            <label class="text-[#0d1b2a] block font-semibold">Poster</label>
+            <input type="text" name="poster" value="{{ $post->poster }}" class="px-2 py-1 border-[#0d1b2a] focus:border-[#0d1b2a] focus:ring-[#0d1b2a] rounded-md shadow-sm mt-1 block w-full">
         </div>
 
         <div>
-            <label class="block font-semibold">Habilitado</label>
-            <select name="habilitated" class="w-full border px-2 py-1">
+            <label class="text-[#0d1b2a] block font-semibold">Habilitado</label>
+            <select name="habilitated" class="px-2 py-1 border-[#0d1b2a] focus:border-[#0d1b2a] focus:ring-[#0d1b2a] rounded-md shadow-sm mt-1 block w-full">
                 <option value="1" {{ $post->habilitated ? 'selected' : '' }}>Sí</option>
                 <option value="0" {{ !$post->habilitated ? 'selected' : '' }}>No</option>
             </select>
         </div>
 
         <div>
-            <label class="block font-semibold">Contenido</label>
-            <textarea name="content" class="w-full border px-2 py-1">{{ $post->content }}</textarea>
+            <label class="text-[#0d1b2a] block font-semibold">Contenido</label>
+            <textarea name="content" class="px-2 py-1 border-[#0d1b2a] focus:border-[#0d1b2a] focus:ring-[#0d1b2a] rounded-md shadow-sm mt-1 block w-full">{{ $post->content }}</textarea>
         </div>
 
-        <button type="submit" class="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-800">
+        <button type="submit" class="bg-[#0d1b2a] text-[#f4f1ee] px-4 py-2 rounded hover:bg-[#f4f1ee] hover:text-[#0d1b2a]">
             Guardar cambios
         </button>
     </form>
+</x-guest-layout>
 @endsection
